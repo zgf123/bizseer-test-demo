1f0f8cd585541137c0c9b860a6db8031110312e9
zhangguangfei <zhangguangfei@bizseer.com>
2023-03-01 16:30:32 +0800

    init

--- src/components/UseDemo.tsx
+++ src/components/UseDemo.tsx
@@ -8,8 +8,11 @@ import ReactFlow, {
   useEdgesState,
   addEdge,
   Panel,
+  Handle,
+  Position,
+  getSmoothStepPath,
 } from 'reactflow'
-// 👇 you need to import the reactflow styles
+// you need to import the reactflow styles
 import 'reactflow/dist/style.css'
 
 const initialNodes = [
@@ -22,24 +25,83 @@ const initialEdges = [
     id: 'e1-2',
     source: '1',
     target: '2',
-    type: 'smoothstep',
+    type: 'custom',
     label: 'aaa',
     pathOptions: { offset: 40, borderRadius: 4 },
   },
 ]
 
-function TextUpdaterNode({ data }: any) {
+const foreignObjectSize = 40
+
+const onEdgeClick = (evt: any, id: any) => {
+  evt.stopPropagation()
+  alert(`remove ${id}`)
+}
+
+function CustomEdge({
+  id,
+  sourceX,
+  sourceY,
+  targetX,
+  targetY,
+  sourcePosition,
+  targetPosition,
+  style = {},
+  data,
+  markerEnd,
+}: any) {
+  const [edgePath] = getSmoothStepPath({
+    sourceX,
+    sourceY,
+    sourcePosition,
+    targetX,
+    targetY,
+    targetPosition,
+  })
+
+  return (
+    <>
+      <path
+        id={id}
+        style={style}
+        className="react-flow__edge-path"
+        d={edgePath}
+        markerEnd={markerEnd}
+      />
+      <foreignObject
+        width={foreignObjectSize}
+        height={foreignObjectSize}
+        x={targetX - 12}
+        y={targetY - 30}
+        className="edgebutton-foreignobject"
+        requiredExtensions="http://www.w3.org/1999/xhtml"
+      >
+        <div>
+          <button className="edgebutton" onClick={(event) => onEdgeClick(event, id)}>
+            x
+          </button>
+        </div>
+      </foreignObject>
+    </>
+  )
+}
+
+function TextUpdaterNode({ data, isConnectable }: any) {
   return (
     <div className="text-updater-node">
-      <div>
+      <div style={{ border: '1px solid red' }}>
         <label htmlFor="text">Text:</label>
         <input id="text" name="text" />
       </div>
+      <Handle type="source" position={Position.Bottom} isConnectable={isConnectable} />
     </div>
   )
 }
 
 const nodeTypes: any = { textUpdater: TextUpdaterNode }
+const edgeTypes = {
+  custom: CustomEdge,
+}
 
 function Flow() {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes)
@@ -62,6 +124,7 @@ function Flow() {
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
         nodeTypes={nodeTypes}
+        edgeTypes={edgeTypes}
         minZoom={0.1}
       >
         <MiniMap nodeStrokeWidth={3} pannable />
