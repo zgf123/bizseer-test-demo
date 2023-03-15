8fd6661130cf95dced5cc03201a436302b356993
zhangguangfei <zhangguangfei@bizseer.com>
2023-03-15 14:06:27 +0800

    radix sort

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-const arr = [6, 7, 8, 5, 9, 3, 4, 1, 2]
+const arr = [6, 7, 811, 5, 9, 31, 4, 1, 2]
 
+var counter = []
 function sort(arr) {
-  if (arr.lengh < 2) return arr
-
+  var mod = 10
+  var dev = 1
+  const maxDigit = Math.ceil(Math.log10(Math.max(...arr)))
+  for (var i = 0; i < maxDigit; i++, dev *= 10, mod *= 10) {
+    for (var j = 0; j < arr.length; j++) {
+      const buketIndex = Math.floor((arr[j] % mod) / dev)
+      counter[buketIndex]
+        ? counter[buketIndex].push(arr[j])
+        : (counter[buketIndex] = []).push(arr[j])
+    }
+    var pos = 0
+    for (var j = 0; j < counter.length; j++) {
+      var value = null
+      if (counter[j] != null) {
+        while ((value = counter[j].shift()) != null) {
+          arr[pos++] = value
+        }
+      }
+    }
+  }
   return arr
 }
 
