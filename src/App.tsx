6a373eea10edd6eab89881ac2f7e99262fb1a6bc
zhangguangfei <zhangguangfei@bizseer.com>
2023-03-21 19:32:50 +0800

    a

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,18 @@
 import React from 'react'
 import FlowDemo from './components/FlowDemo'
 import UseDemo from './components/UseDemo'
+import { Foo } from 'adder-editor'
 
 const state: number = 1
 const App = () => {
-  return <div className="App">{state === 1 ? <FlowDemo /> : <UseDemo />}</div>
+  return (
+    <>
+      <div>
+        <Foo title="heheda" />
+      </div>
+      <div className="App">{state === 1 ? <FlowDemo /> : <UseDemo />}</div>
+    </>
+  )
 }
 
 export default App
