fa7f0d23306bdce0c48c9c31ba63ad40c25e89ee
zhangguangfei <zhangguangfei@bizseer.com>
2023-03-22 09:50:56 +0800

    aa

--- myPlugin.js
+++ myPlugin.js
@@ -2,10 +2,35 @@ const path = require('path')
 const fs = require('fs-extra')
 
 module.exports = (compiler, params) => {
-  compiler.hooks.compile.tap('MyPlugin', (compilation) => {
-    console.log(params)
+  // compiler.hooks.emit.tap('CodeBeautify', (compilation) => {
+  // const assets = compilation.getAssets()
 
-    const fileContent = params.toString()
+  // compilation.hooks.buildModule.tap('SourceMapDevToolModuleOptionsPlugin', (module) => {
+  //   console.log(123)
+  // })
+
+  // Object.keys(compilation.assets).forEach((data) => {
+  //   console.log(data)
+  //   let content = compilation.assets[data].source() // 获取处理的文本
+  //   content = content.replace(reg, function (word) {
+  //     // 去除注释后的文本return /^/{2,}/.test(word) || /^/*!/.test(word) || /^/*{3,}//.test(word) ? "" : word;
+  //   })
+  //   compilation.assets[data] = {
+  //     source() {
+  //       return content
+  //     },
+  //     size() {
+  //       return content.length
+  //     },
+  //   }
+  // })
+  // })
+
+  compiler.hooks.emit.tap('MyPlugin', (compilation) => {
+    console.log(compilation.assets)
+    console.log(JSON.stringify(compilation.assets))
+
+    const fileContent = JSON.stringify(compilation.assets)
     fs.outputFileSync(path.resolve(__dirname, './log.txt'), fileContent)
   })
 }
