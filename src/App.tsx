0ab9a3230c8636809541bc723530682a1bf5344b
zhangguangfei <zhangguangfei@bizseer.com>
2023-04-24 11:17:27 +0800

    aa

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import React from 'react'
 import FlowDemo from './components/FlowDemo'
 import UseDemo from './components/UseDemo'
 import EditorDemo from './components/EditorDemo'
+import AcornDemo from './components/AcornDemo'
 
 const state: number = 1
 const App = () => {
   return (
     <>
       <div>
-        <EditorDemo />
+        <AcornDemo />
       </div>
-      <div className="App">{state === 1 ? <FlowDemo /> : <UseDemo />}</div>
+      {/* <div><EditorDemo /></div> */}
+      {/* <div className="App">{state === 1 ? <FlowDemo /> : <UseDemo />}</div> */}
     </>
   )
 }
---
+++ src/components/AcornDemo.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import * as acorn from 'acorn'
+
+const obj = {
+  b: 1,
+  func() {
+    console.log(this.b)
+  },
+}
+
+obj.func.bind({ b: 2 })()
+const obj2 = {
+  b: 2,
+}
+
+const code = `const fn = a => {
+  let i = 1;
+  return a + i;
+};`
+const ast = acorn.parse(code, { ecmaVersion: 'latest' })
+
+console.log(ast)
+
+// console.log(acorn.Parser(ast))
+
+const AcornDemo = () => {
+  return <>123</>
+}
+
+export default AcornDemo
