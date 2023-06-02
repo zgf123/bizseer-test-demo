1f728df3755b386761e61e3f233c27224ac3b71b
zhangguangfei <zhangguangfei@bizseer.com>
2023-06-02 12:17:56 +0800

    a

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-const arr = [9, 6, 8, 7, 5, 2, 3, 1, 4]
+const arr = [3, 2, -1, 5, 2, 1, 1, 0]
 
 function quickSort(arr, left = 0, right = arr.length - 1) {
   if (left >= right) return arr
-  const index = partition(arr)
-  quickSort(arr, 0, index - 1)
-  quickSort(arr, index + 1, arr.length)
+  const index = partition(arr, left, right)
+  quickSort(arr, left, index - 1)
+  quickSort(arr, index + 1, right)
   return arr
 }
 
 function partition(arr, left, right) {
-  const index = arr(pivot)
+  const pivot = arr[left]
+  let index = left + 1
+  for (let i = index; i <= right; i++) {
+    if (arr[i] < pivot) {
+      ;[arr[i], arr[index]] = [arr[index], arr[i]]
+      index++
+    }
+  }
+  index--
+  ;[arr[left], arr[index]] = [arr[index], arr[left]]
+  return index
 }
 
 console.log(quickSort(arr))
--- note.md
+++ note.md
@@ -1,1 +1,27 @@
 # note
+
+- pragmatic
+- superstition
+- superstitious
+- methane
+- proactive
+- indispensable
+- elastic
+- elasticity
+- aboriginal
+- erosion
+- erode
+- tectonic
+- ascend
+- descend
+
+- gorgeous
+- check
+- sparse
+- frustrate
+- fantasy
+- incinerate
+- deform
+- capillary
+- informative
+- evaporate
