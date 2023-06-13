c0352dcadc9daaabe0806443601afa8c9a48e6ff
zhangguangfei <zhangguangfei@bizseer.com>
2023-06-13 14:00:39 +0800

    feat: coins

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-const arr = [3, 2, -1, 5, 2, 1, 1, 0]
-
-function quickSort(arr, left = 0, right = arr.length - 1) {
-  if (left >= right) return arr
-  const index = partition(arr, left, right)
-  quickSort(arr, left, index - 1)
-  quickSort(arr, index + 1, right)
-  return arr
-}
-
-function partition(arr, left, right) {
-  const pivot = arr[left]
-  let index = left + 1
-  for (let i = index; i <= right; i++) {
-    if (arr[i] < pivot) {
-      ;[arr[i], arr[index]] = [arr[index], arr[i]]
-      index++
+function minCoinChange(coins, sum) {
+  const cache = []
+  for (let i = 0; i < coins.length; i++) {
+    cache[i] = []
+    for (let j = 0; j <= sum; j++) {
+      const amount = j - coins[i]
+      if (amount === 0) {
+        cache[i][j] = 1
+      } else if (amount > 0 && cache[i][amount] > 0) {
+        cache[i][j] = 1 + cache[i][amount]
+      } else {
+        cache[i][j] = i >= 1 ? cache[i - 1][j] : 0
+      }
     }
   }
-  index--
-  ;[arr[left], arr[index]] = [arr[index], arr[left]]
-  return index
+
+  return cache
 }
 
-console.log(quickSort(arr))
+console.log(minCoinChange([1, 3, 5, 8], 58))
