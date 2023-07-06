1d1beeb9ab27d23eccf1dae6fceb9dbf06266682
zhangguangfei <zhangguangfei@bizseer.com>
2023-07-06 10:44:47 +0800

    editor

--- src/components/MonacoDemo.tsx
+++ src/components/MonacoDemo.tsx
@@ -1,7 +1,21 @@
+/** eslint-disable */
 import React from 'react'
+import MonacoEditor from 'react-monaco-editor'
+import 'monaco-editor/esm/vs/basic-languages/javascript/javascript.contribution'
 
 const Demo = () => {
-  return <>monaco-demo</>
+  return (
+    <>
+      <MonacoEditor
+        width="800"
+        height="600"
+        value={`// code...`}
+        onChange={(newValue) => {
+          console.log(newValue)
+        }}
+      />
+    </>
+  )
 }
 
 export default Demo
