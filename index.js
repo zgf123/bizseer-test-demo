29db7290dae9f8ddb1b7b86c048b2d798e2f468f
zhangguangfei <zhangguangfei@bizseer.com>
2023-07-14 20:12:33 +0800

    lcs vector

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 function lcs(s1, s2) {
-  let left = Array.from({ length: s2.length + 1 }, () => [])
+  const vector = [[]]
   for (let i = 1; i <= s1.length; i++) {
-    let temp = [[]]
+    let [prev, cur] = [[], []]
     for (let j = 1; j <= s2.length; j++) {
-      temp = [...temp, left[j]]
+      if (!vector[j]) vector[j] = []
+      ;[prev, cur] = [cur, vector[j]]
       if (s1[i - 1] === s2[j - 1]) {
-        left[j] = [...temp[j - 1], s2[j - 1]]
+        vector[j] = [...prev, s2[j - 1]]
       } else {
-        let a = left[j]
-        let b = left[j - 1]
-        left[j] = a.length > b.length ? a : b
+        let a = vector[j - 1]
+        let b = cur
+        vector[j] = a.length > b.length ? a : b
       }
     }
   }
-
-  return left
+  return vector.pop()
 }
 
-console.log(lcs('abc', 'ab'))
+console.log(lcs('bsbininm', 'jmjkbkjkv'))
