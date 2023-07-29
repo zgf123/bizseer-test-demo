a14571726eb2e66c980d4dacfc68825e6f18154f
zhangguangfei <zhangguangfei@bizseer.com>
2023-07-30 00:08:05 +0800

    feat: layout

--- src/components/react-flow/SubFlowDemo3/tools.ts
+++ src/components/react-flow/SubFlowDemo3/tools.ts
@@ -32,26 +32,26 @@ export type TElements = {
 
 const position = { x: 0, y: 0 }
 export const initialNodes: Node[] = [
-  {
-    id: '1',
-    type: ECustomType.subNode,
-    data: { value: 'Node 1' },
-    position,
-    parentNode: 'g1.2',
-  },
-  {
-    id: '2',
-    type: ECustomType.subNode,
-    data: { value: 'Node 2' },
-    position,
-    parentNode: 'g1.2',
-  },
-  {
-    id: 'g1.2',
-    type: ECustomType.groupNode,
-    data: {},
-    position,
-  },
+  // {
+  //   id: '1',
+  //   type: ECustomType.subNode,
+  //   data: { value: 'Node 1' },
+  //   position,
+  //   parentNode: 'g1.2',
+  // },
+  // {
+  //   id: '2',
+  //   type: ECustomType.subNode,
+  //   data: { value: 'Node 2' },
+  //   position,
+  //   parentNode: 'g1.2',
+  // },
+  // {
+  //   id: 'g1.2',
+  //   type: ECustomType.groupNode,
+  //   data: {},
+  //   position,
+  // },
   {
     id: '3',
     type: ECustomType.subNode,
@@ -74,51 +74,86 @@ export const initialNodes: Node[] = [
     parentNode: 'g3.4',
   },
   {
-    id: 'g3.4',
-    type: ECustomType.groupNode,
-    data: {},
+    id: '6',
+    type: ECustomType.subNode,
+    data: { value: 'Node 6' },
     position,
+    parentNode: 'g3.4',
   },
   {
-    id: 'g3.41',
+    id: '7',
+    type: ECustomType.subNode,
+    data: { value: 'Node 7' },
+    position,
+    parentNode: 'g3.4',
+  },
+  {
+    id: '8',
+    type: ECustomType.subNode,
+    data: { value: 'Node 8' },
+    position,
+    parentNode: 'g3.4',
+  },
+  {
+    id: 'g3.4',
     type: ECustomType.groupNode,
     data: {},
     position,
   },
 ]
 
-for (let i = 0; i < 20; i++) {
+for (let i = 0; i < 0; i++) {
   initialNodes.push({
-    id: 'i' + i,
+    id: 'i-i' + i,
     type: ECustomType.subNode,
-    data: { value: 'Node 5' + i },
+    data: { value: 'Node 5' },
     position,
     parentNode: 'g3.4',
   })
 }
 
+const OtherNodes: Node[] = []
+for (let i = 0; i < 20; i++) {
+  OtherNodes.push({
+    id: 'i' + i,
+    type: ECustomType.subNode,
+    data: { value: 'Node 5' + i },
+    position,
+  })
+}
+
 export const initialEdges: Edge[] = [
-  {
-    id: '1-2',
-    source: '1',
-    target: '2',
-  },
+  // {
+  //   id: '1-2',
+  //   source: '1',
+  //   target: '2',
+  // },
   {
     id: '3-4',
     source: '3',
     target: '4',
   },
   {
-    id: '2-5',
-    source: '2',
+    id: '4-5',
+    source: '4',
     target: '5',
   },
   {
-    id: 'g1.2-g3.4',
-    source: 'g1.2',
-    target: 'g3.4',
-    hidden: true,
+    id: '5-8',
+    source: '5',
+    target: '8',
   },
+  // {
+  //   id: '2-5',
+  //   source: '2',
+  //   target: '5',
+  // },
+  // {
+  //   id: 'g1.2-g3.4',
+  //   source: 'g1.2',
+  //   target: 'g3.4',
+  //   hidden: true,
+  // },
 ]
 
 function genGraph({ nodes = [], edges = [] }: TElements) {
@@ -129,21 +164,73 @@ function genGraph({ nodes = [], edges = [] }: TElements) {
     },
     [[], []]
   )
-  const elk = new ELK()
+  const elk = new ELK({
+    defaultLayoutOptions: {
+      'elk.algorithm': 'layered',
+      'elk.direction': 'RIGHT',
+      'elk.aspectRatio': '0.001',
+      // 'org.eclipse.elk.layered.nodePlacement.strategy': 'BRANDES_KOEPF',
+      // 'org.eclipse.elk.layered.nodePlacement.bk.fixedAlignment': 'LEFTDOWN',
+      // 'org.eclipse.elk.alignment': 'LEFT',
+      // 'org.eclipse.elk.layered.allowNonFlowPortsToSwitchSides': 'true',
+      // 'org.eclipse.elk.commentBox': 'true',
+      // 'org.eclipse.elk.layered.considerModelOrder.strategy': 'PREFER_NODES',
+      // 'org.eclipse.elk.contentAlignment': 'H_LEFT',
+      // 'org.eclipse.elk.layered.directionCongruency': 'ROTATION',
+      // 'org.eclipse.elk.layered.priority.direction': '100',
+      // 'org.eclipse.elk.layered.edgeLabels.centerLabelPlacementStrategy': 'HEAD_LAYER',
+      // 'org.eclipse.elk.layered.nodePlacement.favorStraightEdges': 'true',
+      // 'org.eclipse.elk.nodeSize.fixedGraphSize': 'true',
+      // 'org.eclipse.elk.layered.crossingMinimization.forceNodeModelOrder': 'true',
+      // 'org.eclipse.elk.layered.wrapping.strategy': 'SINGLE_EDGE',
+      // 'org.eclipse.elk.layered.wrapping.correctionFactor': '0.001',
+      // 'org.eclipse.elk.layered.wrapping.cutting.strategy': 'MANUAL',
+      // 'org.eclipse.elk.layered.crossingMinimization.greedySwitch.activationThreshold': '0',
+      // 'org.eclipse.elk.hypernode': 'true',
+      // 'org.eclipse.elk.layered.considerModelOrder.noModelOrder': 'true',
+      // 'org.eclipse.elk.layered.thoroughness': '100',
+      // 'org.eclipse.elk.layered.priority.straightness': '100',
+      // 'org.eclipse.elk.topdown.hierarchicalNodeAspectRatio': '0.01',
+      // 'org.eclipse.elk.topdownLayout': 'true',
+      // 'org.eclipse.elk.layered.layering.minWidth.upperBoundOnWidth': '-1',
+      // 'org.eclipse.elk.layered.wrapping.validify.strategy': 'NO',
+    },
+  })
   const graph: ElkNode = {
     id: 'root',
-    layoutOptions: {
-      'elk.direction': 'RIGHT',
-    },
+    layoutOptions: {},
     children: groupNodes.map((group) => ({
       id: group.id,
-      children: subNodes
-        .filter((node) => node.parentNode === group.id)
-        .map((node) => ({
-          id: node.id,
+      layoutOptions: {},
+      children: [
+        ...subNodes
+          .filter((node) => node.parentNode === group.id)
+          .map((node) => ({
+            id: node.id,
+            width: 100,
+            height: 50,
+          })),
+        // {
+        //   id: 'xxx1',
+        //   layoutOptions: {
+        //     'elk.aspectRatio': '0.001',
+        //     'elk.padding': '[top=0,left=0,bottom=0,right=0]',
+        //   },
+        //   width: 100,
+        //   height: 50,
+        //   children: OtherNodes.map((node) => ({ id: node.id, width: 100, height: 50 })),
+        // },
+        {
+          id: 'xxx2',
+          layoutOptions: {
+            'elk.padding': '[top=0,left=0,bottom=0,right=0]',
+          },
           width: 100,
           height: 50,
-        })),
+          x: 0,
+          children: new Array(3).fill(0).map((item, index) => ({ id: 'sub' + (index + 1), width: 100, height: 50 })),
+        },
+      ],
     })),
     edges: edges.map((edge) => ({
       id: edge.id,
@@ -157,6 +244,7 @@ function genGraph({ nodes = [], edges = [] }: TElements) {
 export async function createLayout(els: TElements) {
   const { elk, graph } = genGraph(els)
   const layout = await elk.layout(graph)
+  console.log(layout)
   const commonOpt = { targetPosition: Position.Left, sourcePosition: Position.Right }
   const nodes = layout.children?.reduce((result: CustomNode[], current) => {
     result.push({
@@ -167,17 +255,40 @@ export async function createLayout(els: TElements) {
       ...commonOpt,
     })
 
-    current.children?.forEach((child) =>
-      result.push({
-        id: child.id,
-        type: ECustomType.subNode,
-        position: { x: child.x || 0, y: child.y || 0 },
-        data: { value: child.id, width: child.width, height: child.height },
-        parentNode: current.id,
-        zIndex: 11,
-        ...commonOpt,
-      })
-    )
+    current.children?.forEach((child) => {
+      if (child.id.startsWith('xxx')) {
+        result.push({
+          id: child.id,
+          type: ECustomType.groupNode,
+          position: { x: child.x || 0, y: child.y || 0 },
+          data: { value: child.id, width: child.width, height: child.height },
+          parentNode: current.id,
+          zIndex: 11,
+          ...commonOpt,
+        })
+        child.children?.forEach((subChild) => {
+          result.push({
+            id: subChild.id,
+            type: ECustomType.subNode,
+            position: { x: subChild.x || 0, y: subChild.y || 0 },
+            data: { value: subChild.id, width: subChild.width, height: subChild.height },
+            parentNode: child.id,
+            zIndex: 11,
+            ...commonOpt,
+          })
+        })
+      } else {
+        result.push({
+          id: child.id,
+          type: ECustomType.subNode,
+          position: { x: child.x || 0, y: child.y || 0 },
+          data: { value: child.id, width: child.width, height: child.height },
+          parentNode: current.id,
+          zIndex: 11,
+          ...commonOpt,
+        })
+      }
+    })
     return result
   }, [])
 
