f1d6df818e494a7ba64bb42e0b006aa00f65a860
zhangguangfei <zhangguangfei@bizseer.com>
2023-10-11 09:48:24 +0800

    feat: ratInMaze

--- index.js
+++ index.js
@@ -1,35 +1,24 @@
-function matrixChainOrder(p) {
-  const n = p.length - 1
-  const m = []
-  const s = []
-  for (let i = 1; i <= n; i++) {
-    m[i] = []
-    m[i][i] = 0
-    s[i] = []
-    for (let j = 1; j <= n; j++) {
-      s[i][j] = 0
-    }
-  }
-  for (let l = 2; l <= n; l++) {
-    for (let i = 1, j = l; j <= n; i++, j++) {
-      m[i][j] = Infinity
-      for (let k = i; k <= j - 1; k++) {
-        const q = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j]
-        if (q < m[i][j]) {
-          m[i][j] = q
-          s[i][j] = k
-        }
-      }
-    }
-  }
-  function fn(s, i, j) {
-    if (i === j) return `A${i}`
-    return `(${fn(s, i, s[i][j])}${fn(s, s[i][j] + 1, j)})`
-  }
-  console.log(fn(s, 1, n))
-  return m[1][n]
+function ratInMaze(maze) {
+  let solution = Array.from({ length: maze.length }, () => new Array(maze[0].length).fill(0))
+  return findPath(maze, solution, 0, 0) ? solution : null
 }
 
-const p = [10, 100, 5, 50, 1]
-console.log(matrixChainOrder(p))
-// 1750
+function findPath(maze, solution, x, y) {
+  let isSafe = x >= 0 && y >= 0 && x < maze.length && y < maze[0].length && maze[x][y] === 1
+  if (isSafe) {
+    solution[x][y] = 1
+    if (x === maze.length - 1 && y === maze[0].length - 1) return true
+    if (findPath(maze, solution, x + 1, y)) return true
+    if (findPath(maze, solution, x, y + 1)) return true
+    solution[x][y] = 0
+  }
+  return false
+}
+const maze = [
+  [1, 1, 1, 1, 1],
+  [0, 0, 0, 1, 1],
+  [1, 1, 1, 1, 1],
+  [1, 0, 0, 1, 0],
+  [1, 1, 1, 1, 1],
+]
+console.log(ratInMaze(maze))
