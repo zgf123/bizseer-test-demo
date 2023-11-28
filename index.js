7a71d4d8640a7a63b50ca4a7464cd08d5489a6a0
zhangguangfei <zhangguangfei@bizseer.com>
2023-11-28 17:40:06 +0800

    aa

--- index.js
+++ index.js
@@ -10,11 +10,65 @@ class BinarySearchTree {
   constructor() {
     this.root = null
   }
-  insert() {}
-  _insert() {}
-  delete() {}
-  _delete() {}
-  inOrderTraverse() {}
+  insert(key) {
+    if (this.root) {
+      this._insert(this.root, key)
+    } else {
+      this.root = new Node(key)
+    }
+  }
+  _insert(node, key) {
+    if (key < node.key) {
+      if (node.left) {
+        this._insert(node.left, key)
+      } else {
+        node.left = new Node(key)
+      }
+    } else {
+      if (node.right) {
+        this._insert(node.right, key)
+      } else {
+        node.right = new Node(key)
+      }
+    }
+  }
+  inOrderTraverse() {
+    this._inOrderTraverse(this.root)
+  }
+  _inOrderTraverse(node) {
+    if (!node) return
+    this._inOrderTraverse(node.left)
+    console.log(node.key)
+    this._inOrderTraverse(node.right)
+  }
+  delete(key) {
+    this.root = this._delete(this.root, key)
+  }
+  _delete(node, key) {
+    if (!node) return null
+    if (key < node.key) {
+      node.left = this._delete(node.left, key)
+      return node
+    } else if (key > node.key) {
+      node.right = this._delete(node.right, key)
+      return node
+    } else {
+      if (node.left && node.right) {
+        let temp = node.left
+        while (temp.right) temp = temp.right
+        node.key = temp.key
+        node.left = this._delete(node.left, temp.key)
+        return node
+      }
+      if (node.left) {
+        return node.left
+      } else if (node.right) {
+        return node.right
+      } else {
+        return null
+      }
+    }
+  }
 }
 
 const tree = new BinarySearchTree()
