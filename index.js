ca56380cf76135d53ead9152f86d458c728f9ce1
zhangguangfei <zhangguangfei@bizseer.com>
2024-01-16 18:07:51 +0800

    feat: RBtree

--- index.js
+++ index.js
@@ -3,78 +3,246 @@ class Node {
     this.key = key
     this.left = null
     this.right = null
+    this.parent = null
+    this.color = 'red'
   }
 }
-
-class BinarySearchTree {
+class RedBlackTree {
   constructor() {
     this.root = null
   }
   insert(key) {
+    let newNode = new Node(key)
     if (this.root) {
-      this._insert(this.root, key)
+      let curNode = this.root
+      while (curNode) {
+        if (key < curNode.key) {
+          if (curNode.left) {
+            curNode = curNode.left
+          } else {
+            curNode.left = newNode
+            newNode.parent = curNode
+            break
+          }
+        } else if (key > curNode.key) {
+          if (curNode.right) {
+            curNode = curNode.right
+          } else {
+            curNode.right = newNode
+            newNode.parent = curNode
+            break
+          }
+        } else {
+          console.log('\x1b[31key值相同')
+          break
+        }
+      }
+      this.insertFix(newNode)
     } else {
-      this.root = new Node(key)
+      this.root = newNode
+      this.root.color = 'black'
     }
   }
-  _insert(node, key) {
-    if (key < node.key) {
-      if (node.left) {
-        this._insert(node.left, key)
+  insertFix(node) {
+    let curNode = node
+    while (curNode.color === 'red' && curNode.parent?.color === 'red') {
+      let parent = curNode.parent
+      let grandParent = parent.parent
+      if (parent === grandParent.left) {
+        let uncle = grandParent.right
+        if (uncle?.color === 'red') {
+          parent.color = 'black'
+          uncle.color = 'black'
+          grandParent.color = 'red'
+          curNode = grandParent
+        } else {
+          if (curNode === parent.right) {
+            this.rotateRR(parent)
+            ;[curNode, parent] = [parent, curNode]
+          }
+          parent.color = 'balck'
+          grandParent.color = 'red'
+          this.rotateLL(grandParent)
+        }
       } else {
-        node.left = new Node(key)
-      }
-    } else {
-      if (node.right) {
-        this._insert(node.right, key)
-      } else {
-        node.right = new Node(key)
+        let uncle = grandParent.left
+        if (uncle?.color === 'red') {
+          parent.color = 'black'
+          uncle.color = 'black'
+          grandParent.color = 'red'
+          curNode = grandParent
+        } else {
+          if (curNode === parent.left) {
+            this.rotateLL(parent)
+            ;[curNode, parent] = [parent, curNode]
+          }
+          parent.color = 'black'
+          grandParent.color = 'red'
+          this.rotateRR(grandParent)
+        }
       }
     }
-  }
-  inOrderTraverse() {
-    this._inOrderTraverse(this.root)
-  }
-  _inOrderTraverse(node) {
-    if (!node) return
-    this._inOrderTraverse(node.left)
-    console.log(node.key)
-    this._inOrderTraverse(node.right)
+    this.root.color = 'black'
   }
   delete(key) {
-    this.root = this._delete(this.root, key)
-  }
-  _delete(node, key) {
-    if (!node) return null
-    if (key < node.key) {
-      node.left = this._delete(node.left, key)
-      return node
-    } else if (key > node.key) {
-      node.right = this._delete(node.right, key)
-      return node
-    } else {
-      if (node.left && node.right) {
-        let temp = node.left
-        while (temp.right) temp = temp.right
-        node.key = temp.key
-        node.left = this._delete(node.left, temp.key)
-        return node
-      }
-      if (node.left) {
-        return node.left
-      } else if (node.right) {
-        return node.right
+    if (!this.root) return null
+    let curNode = this.root
+    while (curNode) {
+      if (key < curNode.key) {
+        curNode = curNode.left
+      } else if (key > curNode.key) {
+        curNode = curNode.right
       } else {
-        return null
+        break
       }
     }
+    if (!curNode) return null
+    if (curNode.left && curNode.right) {
+      let temp = curNode.left
+      while (temp.right) temp = temp.right
+      curNode.key = temp.key
+      curNode = temp
+    }
+
+    let subNode = curNode.left || curNode.right
+    if (curNode.parent) {
+      if (subNode) {
+        subNode.parent = curNode.parent
+        if (curNode === curNode.parent.left) {
+          curNode.parent.left = subNode
+        } else {
+          curNode.parent.right = subNode
+        }
+        if (curNode.color === 'black') subNode.color = 'black'
+      } else {
+        this.deleteFix(curNode)
+        if (curNode === curNode.parent.left) {
+          curNode.parent.left = null
+        } else {
+          curNode.parent.right = null
+        }
+      }
+    } else {
+      if (subNode) {
+        this.root = subNode
+        this.root.color = 'black'
+      } else {
+        this.root = null
+      }
+    }
+  }
+  deleteFix(node) {
+    let curNode = node
+    while (curNode !== this.root && curNode.color === 'black') {
+      if (curNode === curNode.parent.left) {
+        let brother = curNode.parent.right
+        if (brother.color === 'red') {
+          brother.color = 'black'
+          brother.parent.color = 'red'
+          this.rotateRR(brother.parent)
+          brother = curNode.parent.right
+        }
+        if (this.isBlack(brother.left) && this.isBlack(brother.right)) {
+          brother.color = 'red'
+          curNode = curNode.parent
+        } else {
+          if (this.isBlack(brother.right)) {
+            brother.color = 'red'
+            brother.left.color = 'black'
+            this.rotateLL(brother)
+            brother = curNode.parent.right
+          }
+          brother.color = curNode.parent.color
+          brother.right.color = 'black'
+          curNode.parent.color = 'black'
+          this.rotateRR(curNode.parent)
+          curNode = this.root
+        }
+      } else {
+        let brother = curNode.parent.left
+        if (brother.color === 'red') {
+          brother.color = 'black'
+          brother.parent.color = 'red'
+          this.rotateLL(brother.parent)
+          brother = curNode.parent.left
+        }
+        if (this.isBlack(brother.left) && this.isBlack(brother.right)) {
+          brother.color = 'red'
+          curNode = curNode.parent
+        } else {
+          if (this.isBlack(brother.left)) {
+            brother.color = 'red'
+            brother.right.color = 'black'
+            this.rotateRR(brother)
+            brother = curNode.parent.left
+          }
+          brother.color = curNode.parent.color
+          brother.left.color = 'black'
+          curNode.parent.color = 'black'
+          this.rotateLL(curNode.parent)
+          curNode = this.root
+        }
+      }
+    }
+    curNode.color = 'black'
+  }
+
+  isBlack(node) {
+    return node ? node.color === 'black' : true
+  }
+  rotateLL(node) {
+    let temp = node.left
+    temp.parent = node.parent
+    if (node.parent) {
+      if (node === node.parent.left) {
+        node.parent.left = temp
+      } else {
+        node.parent.right = temp
+      }
+    } else {
+      this.root = temp
+    }
+    node.left = temp.right
+    if (temp.right) temp.right.parent = node
+    temp.right = node
+    node.parent = temp
+  }
+  rotateRR(node) {
+    let temp = node.right
+    temp.parent = node.parent
+    if (node.parent) {
+      if ((node = node.parent.left)) {
+        node.parent.left = temp
+      } else {
+        node.parent.right = temp
+      }
+    } else {
+      this.root = temp
+    }
+    node.right = temp.left
+    if (temp.left) temp.left.parent = node
+    temp.left = node
+    node.parent = temp
+  }
+  print() {
+    if (!this.root) return ''
+    let arr = [this.root, 'br']
+    let str = ''
+    while (arr.length) {
+      let item = arr.shift()
+      str += item === 'br' ? '\n' : `${item.color === 'red' ? '\x1b[31m' : ''}${item.key}\x1b[0m `
+      if (item.left) arr.push(item.left)
+      if (item.right) arr.push(item.right)
+      if (arr.length && item === 'br') arr.push('br')
+    }
+    console.log(str)
   }
 }
 
-const tree = new BinarySearchTree()
-let arr = [6, 3, 12, 2, 1, 5, 4, 8, 9, 7, 10, 15, 14, 13, 11]
+let arr = [7, 6, 3, 9, 4, 1, 2, 5]
+let tree = new RedBlackTree()
 arr.forEach((n) => tree.insert(n))
-tree.delete(6)
-tree.delete(5)
+tree.delete(4)
 tree.delete(7)
-tree.inOrderTraverse()
+tree.delete(6)
+tree.print()
